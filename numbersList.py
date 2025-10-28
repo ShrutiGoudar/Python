c9fa6e1571b53e476caf2082a21b0c886813e78a
ShrutiGoudar <shrt.goudar@gmail.com>
2025-10-28 15:20:14 +0530

    Numbers List practice : random generation

---
+++ numbersList.py
@@ -0,0 +1,22 @@
+# Get Largest Number in List
+# Write a Python program to get the largest number from a list.
+import random
+list1 =[]
+for i in range(10):
+    list1.append(random.randint(-100,100)) #random integer generation
+
+print(list1)
+#logical implementation
+largest = list1[0]
+for i in range(len(list1)) :
+    if(list1[i] > largest):
+        largest = list1[i] 
+print(f"Largest number : {largest}")
+# Get Smallest Number in List
+# Write a Python program to get the smallest number from a list.
+
+smallest = list1[0]
+for i in range(len(list1)) :
+    if(list1[i] < smallest):
+        smallest = list1[i] 
+print(f"smallest number : {smallest}")
